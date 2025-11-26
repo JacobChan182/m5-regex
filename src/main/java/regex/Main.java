0d77d53b7777cfd404af7578070eae6f154be419
JacobChan182 <jacobchanemail@gmail.com>
2025-11-25 22:53:02 -0500

    fixed empty string bug

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -38,6 +38,9 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
+        if (str == null) {
+            return false;
+        }
         final boolean propertyOne = Pattern.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{" + minLength + ",}", str);
         // as needed, modify this code.
         return propertyOne;
@@ -55,6 +58,9 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
+        if (str == null) {
+            return new ArrayList<>();
+        }
         final Pattern pattern = Pattern.compile("\\b[A-Za-z0-9._%+-]+@(mail\\.utoronto\\.ca|utoronto\\.ca)\\b");
         final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
@@ -76,6 +82,9 @@ public class Main {
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
+        if (str == null) {
+            return false;
+        }
         return str.matches(".*([A-Z]).*\\1.*");
     }
 }
